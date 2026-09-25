249a512ad408b841273acae0b5f8a48293482a81
2022wa86032 <2022wa86032@wilp.bits-pilani.ac.in>
2026-09-25 06:06:34 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven!");
+        System.out.println("Jenkins Continuous Integration Demo");
+    }
+}
