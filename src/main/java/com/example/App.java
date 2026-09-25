48464b09ea08c0e8de2d8f807398dad942b006eb
2022wa86032 <2022wa86032@wilp.bits-pilani.ac.in>
2026-09-25 07:06:15 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,8 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Maven!");
-        System.out.println("Jenkins Continuous Integration Demo");
+        System.out.println("Hello from Jenkins CI!");
+        System.out.println("Continuous Integration Build Successful");
+
     }
 }
